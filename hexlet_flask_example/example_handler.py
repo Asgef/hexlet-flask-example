bef1b181186a1fcad28bdbf7aab336025c7566ee
Asgef <asgefes1@gmail.com>
2024-03-08 17:00:10 +0300

    Study the lesson "Dynamic routes"

--- hexlet_flask_example/example_handler.py
+++ hexlet_flask_example/example_handler.py
@@ -1,19 +1,10 @@
-from flask import make_response, Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/foo')
-def foo():
-    response = make_response('foo')
-    # Устанавливаем заголовок
-    response.headers['X-Parachutes'] = 'parachutes are cool'
-    # Меняем тип ответа
-    response.mimetype = 'text/plain'
-    # Задаем статус
-    response.status_code = 418
-    # Устанавливаем cookie
-    response.set_cookie('foo', 'bar')
-    return response
+@app.route('/courses/<id>')
+def courses(id):
+    return f'Course id: {id}'
 
 
