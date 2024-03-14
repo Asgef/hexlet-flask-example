19e5745940e1252bda06a290046306a0b6364b98
Asgef <asgefes1@gmail.com>
2024-03-14 13:46:51 +0300

    Study "Modifying forms"

---
+++ hexlet_flask_example/example_form.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect
+from random import randint
+from json import dumps
+import os
+
+
+app = Flask(__name__)
+
+
+@app.post('/users')
+def users_post():
+    user_db_path = os.path.join(os.path.dirname(__file__), 'templates', 'users', 'user_db')
+    with open(user_db_path, 'w') as repo:
+        user = request.form.to_dict()
+        id = randint(100, 999)
+        user['id'] = id
+        repo.write(dumps(user))
+    return redirect('/users', code=302)
+
+
+@app.route('/users/new')
+def users_new():
+    user = {'name': '',
+            'email': '',
+            }
+
+    return render_template(
+        'users/new.html',
+        user=user
+    )
