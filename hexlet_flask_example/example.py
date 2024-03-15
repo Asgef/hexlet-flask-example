8065f326c42a53319bd0b4969d56fe429f3041c8
Asgef <asgefes1@gmail.com>
2024-03-15 15:27:51 +0300

    Learn a lesson on CRUD
    Modify module example.py
    Create a page with a list of courses, with links to each course.
    Put into practice 'Named routes', using json, accessing a file.

--- hexlet_flask_example/example.py
+++ hexlet_flask_example/example.py
@@ -1,22 +1,44 @@
 from flask import Flask, render_template
+import os
+import json
 
 # Это callable WSGI-приложение
 app = Flask(__name__)
 
 
-@app.route('/users/<id>')
-def users(id):
+@app.route('/courses')
+def get_courses():
+    courses_db_path = os.path.join(
+        os.path.dirname(__file__), 'templates', 'courses', 'courses_db.json'
+    )
+    with open(courses_db_path, 'r') as repo:
+        content = repo.read()
+        if content:
+            courses = json.loads(content)
+        else:
+            courses = []
 
     return render_template(
-        'index.html',
-        name=id,
-    )
+           'courses/index.html',
+           courses=courses,
+           )
 
 
 @app.route('/courses/<id>')
-def courses(id):
+def get_course(id):
+    courses_db_path = os.path.join(
+        os.path.dirname(__file__), 'templates', 'courses', 'courses_db.json'
+    )
+    with open(courses_db_path, 'r') as repo:
+        content = repo.read()
+        courses = json.loads(content) if content else []
+
+    # Поиск курса по ID
+    course = next((item for item in courses if item.get("id") == id), None)
+    if course is None:
+        return 'Page not found', 404
 
     return render_template(
-        'show.html',
-        id=id,
-    )
+          'courses/show.html',
+          course=course,
+          )
--- hexlet_flask_example/example_form.py
+++ hexlet_flask_example/example_form.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import (
+    Flask, render_template,
+    request, redirect, url_for,
+    flash, get_flashed_messages
+)
 from random import randint
 from json import dumps
 import os
 
 
 app = Flask(__name__)
+app.secret_key = "secret_key"
 
 
 @app.post('/users')
 def users_post():
     user_db_path = os.path.join(os.path.dirname(__file__), 'templates', 'users', 'user_db')
-    with open(user_db_path, 'w') as repo:
+    with open(user_db_path, 'a') as repo:
         user = request.form.to_dict()
         id = randint(100, 999)
         user['id'] = id
         repo.write(dumps(user))
-    return redirect(url_for('users_post'), code=302)
+        flash('New user added', 'success')
+    return redirect(url_for('users_new'), code=302)
 
 
 @app.route('/users/new')
@@ -23,8 +29,11 @@ def users_new():
     user = {'name': '',
             'email': '',
             }
+    messages = get_flashed_messages(with_categories=True)
+    print(messages)
 
     return render_template(
         'users/new.html',
-        user=user
+        user=user,
+        messages=messages
     )
